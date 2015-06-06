f70965a7f244b7a86423c2efcb339f9a145a15f8
Gambya <robertolopesramos@gmail.com>
2015-06-05 23:36:40 -0300

    Implementação do requisito cadastro de stackholder.

---
+++ SIGREQ/SIGREQ/Controller/StackholderController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SIGREQ.Model;
+using System.Collections;
+
+namespace SIGREQ.Controller
+{
+    public class StackholderController
+    {
+        public List<STACKHOLDER> GetAll()
+        {
+            using (sigreqEntities ctx = new sigreqEntities())
+            {
+                return ctx.STACKHOLDER.ToList();
+            }
+        }
+
+        public IEnumerable GetStackholders()
+        {
+            using (sigreqEntities ctx = new sigreqEntities())
+            {
+                return ctx.STACKHOLDER.Select(s=>new {
+                    ID = s.STACKHOLDERID,
+                    Nome = s.NOME,
+                    Tipo = s.TIPOSTACKHOLDER.NOME,
+                    Telefone = s.TELEFONE,
+                    Registro = s.DATAREGISTRO
+                }).ToList();
+            }
+        }
+
+        public STACKHOLDER GetStackholder(int stackholderid)
+        {
+            using (sigreqEntities ctx = new sigreqEntities())
+            {
+                return ctx.STACKHOLDER.Where(s => s.STACKHOLDERID == stackholderid).FirstOrDefault();
+            }
+        }
+
+        public void InserirStackholder(STACKHOLDER stackholder)
+        {
+            using(sigreqEntities ctx = new sigreqEntities())
+            {
+                ctx.STACKHOLDER.Add(stackholder);
+                ctx.SaveChanges();
+            }
+        }
+    }
+}
---
+++ SIGREQ/SIGREQ/Controller/TipoStackholderController.cs
@@ -0,0 +1,38 @@
+﻿using SIGREQ.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIGREQ.Controller
+{
+    public class TipoStackholderController
+    {
+        public List<TIPOSTACKHOLDER> GetAll()
+        {
+            using(sigreqEntities ctx = new sigreqEntities())
+            {
+                var tstackholder = ctx.TIPOSTACKHOLDER.ToList();
+                return tstackholder;
+            }
+        }
+
+        public TIPOSTACKHOLDER GetTipoStackholder(int tipoStackholderId)
+        {
+            using(sigreqEntities ctx = new sigreqEntities())
+            {
+                return ctx.TIPOSTACKHOLDER.Where(t => t.TIPOSTACKHOLDERID == tipoStackholderId).FirstOrDefault();
+            }
+        }
+
+        public void InserirTipoStackholder(TIPOSTACKHOLDER tipoStackholder)
+        {
+            using (sigreqEntities ctx = new sigreqEntities())
+            {
+                ctx.TIPOSTACKHOLDER.Add(tipoStackholder);
+                ctx.SaveChanges();
+            }
+        }
+    }
+}
